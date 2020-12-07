1edaec5b0fa600100dfe192f7405ce91efd2b2de
montipallarols <montaga.pallarols@gmail.com>
2020-12-07 16:55:14 +0100

    Install ract navigation and set up HomeScreen

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from "./src/screens/HomeScreen";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>AntiStress</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View style={{ alignItems: "center", justifyContent: "center", flex: 1 }}>
+      <Text
+        style={{
+          fontWeight: "bold",
+          fontSize: 30,
+          textAlign: "center",
+        }}
+      >
+        Welcome to AntiStress!
+      </Text>
+    </View>
+  );
+}

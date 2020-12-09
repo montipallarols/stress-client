3fa30ba624ddfa210a6845e3a260f79136aa5025
Baskoekkoek1 <baskoekkoek@gmail.com>
2020-12-09 10:20:00 +0100

    fix token bug

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,18 +2,19 @@ import React from "react";
 import { Text, View, Button } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 import { logOut } from "../store/user/actions";
-import { selectToken } from "../store/user/selectors";
+import { selectToken, selectUser } from "../store/user/selectors";
 
 export default function HomeScreen({ navigation }) {
   const dispatch = useDispatch();
-  const token = useSelector(selectToken);
+  const user = useSelector(selectUser);
+  const token = user.token;
 
-
-  function onPressLogout(){
-    console.log("logout")
-    dispatch(logOut())
+  function onPressLogout() {
+    console.log("logout");
+    dispatch(logOut());
   }
- 
+  console.log("token", token);
+
   return (
     <View style={{ alignItems: "center", justifyContent: "center", flex: 1 }}>
       <Text
@@ -34,22 +35,21 @@ export default function HomeScreen({ navigation }) {
       >
         How is everyone feeling today?
       </Text>
- 
-      { token ? 
-      <Button
-        title="My Profile"
-        onPress={() => navigation.navigate("My Profile")}
-      /> : 
-      <Button title="SignUp" onPress={() => navigation.navigate("SignUp")} />
-      }
-     
-      { token ? 
-      <Button title="Logout" onPress={onPressLogout} /> :
-      <Button title="Login" onPress={() => navigation.navigate("Login")} /> 
-    }
-    
-      
-      
+
+      {token ? (
+        <Button
+          title="My Profile"
+          onPress={() => navigation.navigate("My Profile")}
+        />
+      ) : (
+        <Button title="SignUp" onPress={() => navigation.navigate("SignUp")} />
+      )}
+
+      {token ? (
+        <Button title="Logout" onPress={onPressLogout} />
+      ) : (
+        <Button title="Login" onPress={() => navigation.navigate("Login")} />
+      )}
     </View>
   );
 }
